be7f327ce236302b2f17c045bd73fc1ca61babef
arnoldbro <74732309+arnoldbro@users.noreply.github.com>
2021-01-19 13:32:53 -0500

    Update 第七作业.js

--- Class9/第七作业.js
+++ Class9/第七作业.js
@@ -1,4 +1,3 @@
-
 // 第一//
 
 let 约翰的质量 = 92;
@@ -31,7 +30,10 @@ var 卢卡斯的HigherBMI二 = 卢卡斯的BMI二 > 约翰的BMI二;
 
 console.log("第二:", "\n", + 英文名字John + "的BMI是" + 约翰的BMI二 + "kg/m^2.", 英文名字Lucas + "的BMI是" + 卢卡斯的BMI二 + "kg/m^2.", "\n", "卢卡斯的BMI高于约翰的: " + 卢卡斯的HigherBMI二);
 
-//Template literal Practice//
+
+
+
+
 
 let 新约翰的质量 = 85;
 let 新约翰的身高 = 1.76;
@@ -44,4 +46,4 @@ var 新约翰的BMI = 新约翰的质量 / 约翰的身高 * 2;
 let 新数据 = `卢卡斯和约翰是朋友. 卢卡斯身高${新卢卡斯的身高}m. 约翰身高${约翰的身高}m. 因此，约翰的BMI高于卢卡斯的. 它是${新约翰的BMI}kg/m.`;
 
 
-console.log("新数据:", "\n" + 新数据);
+console.log("新数据:", "\n", 新数据);
