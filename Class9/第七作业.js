f2f320ba68a33c4e56a9190b88139b2813271b36
arnoldbro <74732309+arnoldbro@users.noreply.github.com>
2021-01-19 13:16:59 -0500

    Update 第七作业.js

--- Class9/第七作业.js
+++ Class9/第七作业.js
@@ -21,30 +21,27 @@ console.log("第一:", "\n", 英文名字John + "的BMI是" + 约翰的BMI + "kg
 
 // 第二//
 
-var 约翰的BMITwo = 约翰的质量 / (约翰的身高 * 约翰的身高);
-约翰的BMITwo = 约翰的BMITwo.toFixed(2);
+var 约翰的BMI二 = 约翰的质量 / (约翰的身高 * 约翰的身高);
+约翰的BMI二 = 约翰的BMI二.toFixed(2);
 
 
-var 卢卡斯的BMITwo = 卢卡斯的质量 / (卢卡斯的身高 * 卢卡斯的身高);
-卢卡斯的BMITwo = 卢卡斯的BMITwo.toFixed(2);
-var 卢卡斯的HigherBMITwo = 卢卡斯的BMITwo > 约翰的BMITwo;
+var 卢卡斯的BMI二 = 卢卡斯的质量 / (卢卡斯的身高 * 卢卡斯的身高);
+卢卡斯的BMI二 = 卢卡斯的BMI二.toFixed(2);
+var 卢卡斯的HigherBMI二 = 卢卡斯的BMI二 > 约翰的BMI二;
 
-console.log("第二:", "\n" + 英文名字John + "的BMI是" + 约翰的BMITwo + "kg/m^2.", 英文名字Lucas + "的BMI是" + 卢卡斯的BMITwo + "kg/m^2.", "\n", "卢卡斯的BMI高于约翰的: " + 卢卡斯的HigherBMITwo);
+console.log("第二:", "\n" + 英文名字John + "的BMI是" + 约翰的BMI二 + "kg/m^2.", 英文名字Lucas + "的BMI是" + 卢卡斯的BMI二 + "kg/m^2.", "\n", "卢卡斯的BMI高于约翰的: " + 卢卡斯的HigherBMI二);
 
 //Template literal Practice//
 
-let 约翰的质量New = 85;
-let 约翰的身高New = 1.76;
-let 卢卡斯的质量New = 95;
-let 卢卡斯的身高New = 1.88;
+let 新约翰的质量 = 85;
+let 新约翰的身高 = 1.76;
+let 新卢卡斯的质量 = 95;
+let 新卢卡斯的身高 = 1.88;
 
-var 约翰的BMINew = 约翰的质量New / 约翰的身高New * 2;
-约翰的BMINew = 约翰的BMINew.toFixed(2);
+var 新约翰的BMI = 新约翰的质量 / 约翰的身高 * 2;
+新约翰的BMI = 新约翰的BMI.toFixed(2);
 
-let 新数据 = `卢卡斯和约翰是朋友. 卢卡斯身高${卢卡斯的身高New}m. 约翰身高${约翰的身高New}m. 因此，约翰的BMI高于卢卡斯的. 它是 ${约翰的BMINew}kg/m.`;
+let 新数据 = `卢卡斯和约翰是朋友. 卢卡斯身高${新卢卡斯的身高}m. 约翰身高${约翰的身高}m. 因此，约翰的BMI高于卢卡斯的. 它是${新约翰的BMI}kg/m.`;
 
 
 console.log("新数据:", "\n" + 新数据);
-
-
-
